7d583510fd63344e6c99e599622de53a868310b0
DESKTOP-7J1U5PL\和馬 <b1014155@gmail.com>
2017-07-07 09:58:42 +0900

    [add] newAccount画面

---
+++ app/src/main/java/com/funney/funney_android/NewAccount.java
@@ -0,0 +1,15 @@
+package com.funney.funney_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+
+        setTitle("FUNney");
+    }
+}
