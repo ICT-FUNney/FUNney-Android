7501b76aaa89b86ab2030e1c3807b841da5e0aac
shinketya <shinketya@gmail.com>
2017-07-22 19:13:50 +0900

    [add] 利用規約登録画面、追加

---
+++ app/src/main/java/com/funney/funney_android/Kiyaku_touroku.java
@@ -0,0 +1,13 @@
+package com.funney.funney_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Kiyaku_touroku extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kiyaku_touroku);
+    }
+}
