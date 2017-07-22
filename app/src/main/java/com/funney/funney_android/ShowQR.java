4e27f13b4c14b4fe36780c8ba72e0dbc100b266d
Keita <bonjovi1926@yahoo.co.jp>
2017-07-22 16:11:06 +0900

    add: ShowQR activity

--- app/src/main/java/com/funney/funney_android/ShowQR.java
+++ app/src/main/java/com/funney/funney_android/ShowQR.java
@@ -3,11 +3,101 @@ package com.funney.funney_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//import android.support.v4.app.FragmentActivity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+//import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+
 public class ShowQR extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_qr);
+
+        View v = getWindow().getDecorView();
+        onClickQRCodeCreate(v);
+    }
+
+    //QRCode作成
+    public void onClickQRCodeCreate(View view)
+    {
+        // QRCodeの作成
+        Bitmap qrCodeBitmap = this.createQRCode("ICT-FUNNEY");
+
+        // QRCodeの作成に成功した場合
+        if (qrCodeBitmap != null)
+        {
+            // 結果をImageViewに表示
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageBitmap(qrCodeBitmap);
+        }
+    }
+
+    private Bitmap createQRCode(String contents)
+    {
+        Bitmap qrBitmap = null;
+        try {
+            // QRコードの生成
+            QRCodeWriter qrcodewriter = new QRCodeWriter();
+            BitMatrix qrBitMatrix = qrcodewriter.encode(contents,
+                    BarcodeFormat.QR_CODE,
+                    300,
+                    300);
+
+            qrBitmap = Bitmap.createBitmap(300, 300, Bitmap.Config.ARGB_8888);
+            qrBitmap.setPixels(this.createDot(qrBitMatrix), 0, 300, 0, 0, 300, 300);
+        }
+        catch(Exception ex)
+        {
+            // エンコード失敗
+            Toast.makeText(getApplicationContext(), ex.toString(), Toast.LENGTH_SHORT).show();
+        }
+        finally
+        {
+            return qrBitmap;
+        }
+    }
+
+    // ドット単位の判定
+    private int[] createDot(BitMatrix qrBitMatrix)
+    {
+        // 縦幅・横幅の取得
+        int width = qrBitMatrix.getWidth();
+        int height = qrBitMatrix.getHeight();
+        // 枠の生成
+        int[] pixels = new int[width * height];
+
+        // データが存在するところを黒にする
+        for (int y = 0; y < height; y++)
+        {
+            // ループ回数盤目のOffsetの取得
+            int offset = y * width;
+            for (int x = 0; x < width; x++)
+            {
+                // データが存在する場合
+                if (qrBitMatrix.get(x, y))
+                {
+                    pixels[offset + x] = Color.BLACK;
+                }
+                else
+                {
+                    pixels[offset + x] = Color.WHITE;
+                }
+            }
+        }
+        // 結果を返す
+        return pixels;
     }
 }
