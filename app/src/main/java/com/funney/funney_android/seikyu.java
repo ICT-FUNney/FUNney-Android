945deed69714fdc0570b273a04d74b4698711ab2
hichioka <b1017129@fun.ac.jp>
2017-07-22 14:53:49 +0900

    [add]メール送金画面追加+請求画面の追加

---
+++ app/src/main/java/com/funney/funney_android/seikyu.java
@@ -0,0 +1,18 @@
+package com.funney.funney_android;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class seikyu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login
+        );
+
+        setTitle("FUNney");
+    }
+}
+
