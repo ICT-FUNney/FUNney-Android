c068fa10e77a7791b24fb551f0f89a3f6e5f98dc
shinketya <shinketya@gmail.com>
2017-07-22 19:06:50 +0900

    [add]  アカウント情報画面追加

---
+++ app/src/main/java/com/funney/funney_android/Account_information.java
@@ -0,0 +1,13 @@
+package com.funney.funney_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Account_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_information);
+    }
+}
