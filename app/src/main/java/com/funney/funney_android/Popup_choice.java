1514522cc464dd044f321db98f3d4c855f9a9bab
Toshimi <toshimi0218@icloud.com>
2017-07-23 11:17:36 +0900

    ヘルプオプション画面の追加

---
+++ app/src/main/java/com/funney/funney_android/Popup_choice.java
@@ -0,0 +1,13 @@
+package com.funney.funney_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Popup_choice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup_choice);
+    }
+}
