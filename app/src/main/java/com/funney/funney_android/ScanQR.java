6d19fb8514852e94fccd9fae8fb1d3accfe6f276
manohito <fun.masahiro@gmail.com>
2017-10-10 16:20:25 +0900

    ScanQRの不要なインポートを除去

--- app/src/main/java/com/funney/funney_android/ScanQR.java
+++ app/src/main/java/com/funney/funney_android/ScanQR.java
@@ -1,15 +1,9 @@
 package com.funney.funney_android;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
