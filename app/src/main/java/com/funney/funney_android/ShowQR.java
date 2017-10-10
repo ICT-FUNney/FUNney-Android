ee12ec6b92d7b094fc0e9067d566a1272f072a65
manohito <fun.masahiro@gmail.com>
2017-10-10 16:17:18 +0900

    不要なインポート除去とリファクタリング

--- app/src/main/java/com/funney/funney_android/ShowQR.java
+++ app/src/main/java/com/funney/funney_android/ShowQR.java
@@ -1,17 +1,14 @@
 package com.funney.funney_android;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.util.Log;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-import android.view.View.OnClickListener;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.common.BitMatrix;
@@ -66,9 +63,8 @@ public class ShowQR extends AppCompatActivity {
         } catch (Exception ex) {
             // エンコード失敗
             Toast.makeText(getApplicationContext(), ex.toString(), Toast.LENGTH_SHORT).show();
-        } finally {
-            return qrBitmap;
         }
+        return qrBitmap;
     }
 
     // ドット単位の判定
