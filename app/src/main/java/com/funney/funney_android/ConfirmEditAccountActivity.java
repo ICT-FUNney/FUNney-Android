435b9668ed9a87eca8fbdeab383993c8ad05059f
Keita <bonjovi1926@yahoo.co.jp>
2017-10-17 16:08:39 +0900

    add: ConfirmEditAccountActivity

---
+++ app/src/main/java/com/funney/funney_android/ConfirmEditAccountActivity.java
@@ -0,0 +1,13 @@
+package com.funney.funney_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfirmEditAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_edit_account);
+    }
+}
