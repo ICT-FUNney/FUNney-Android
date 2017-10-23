373763be033b38e6245fe47b4feaa0bf039fe51e
hichioka <b1017129@fun.ac.jp>
2017-10-23 18:13:16 +0900

    アカウント情報入力画面を作った

--- app/src/main/java/com/funney/funney_android/Pay.java
+++ app/src/main/java/com/funney/funney_android/Pay.java
@@ -1,20 +1,19 @@
 package com.funney.funney_android;
 
-import android.content.Intent;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class Pay extends AppCompatActivity {
 
-    private TextView textView;
+
     private DialogFragment dialogFragment;
     private FragmentManager flagmentManager;
 
@@ -23,7 +22,6 @@ public class Pay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
 
-        textView = (TextView) findViewById(R.id.alert_view);
 
         Button requestButton = (Button) findViewById(R.id.request);
         requestButton.setOnClickListener(new View.OnClickListener()
@@ -37,13 +35,8 @@ public class Pay extends AppCompatActivity {
 
         Button sendButton = (Button) findViewById(R.id.send);
         sendButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
 
-                //ここでQRカメラ起動
-            }
-        });
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
+            //アラートの起動
             @Override
             public void onClick(View v) {
                 flagmentManager = getSupportFragmentManager();
@@ -51,56 +44,46 @@ public class Pay extends AppCompatActivity {
                 // DialogFragment を継承したAlertDialogFragmentのインスタンス
                 dialogFragment = new AlertDialogFragment();
                 // DialogFragmentの表示
-                dialogFragment.show(flagmentManager, "how would you like to send money?");
+                dialogFragment.show(flagmentManager, "alert dialog");
             }
         });
     }
 
-    public void setTextView(String message) {
-        textView.setText(message);
-    }
 
     // DialogFragment を継承したクラス
     public static class AlertDialogFragment extends DialogFragment {
-        // 選択肢のリスト
-        private String[] menulist = {"Scan a QR Code", "Send to email or contact", "キャンセル"};
+        // アラートの選択肢のリスト
+        private String[] menulist = {"Scan a QR Code", "Send to email or contact","cancel"};
 
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
 
             AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
 
-            // タイトル
-            alert.setTitle("Test AlertDialog");
+            alert.setTitle("How would you like to send money?");
             alert.setItems(menulist, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int idx) {
-                    // 選択１
-                    if (idx == 0) {
-                        setMassage(menulist[0]);
-                    }
-                    // 選択２
-                    else if (idx == 1) {
-                        setMassage(menulist[1]);
-                    }
-                    // 選択３
-                    else if (idx == 2) {
-                        setMassage(menulist[2]);
-                    }
-                    // cancel"
-                    else {
+
+                    // アラートの選択肢
+                    //if (idx == 0) {
+                        //アラートを押したら画面遷移
+                       Intent sendIntent = new Intent(getActivity().getApplication(), ShowQR.class);
+                        startActivity(sendIntent);
+                    //}
+                   // else if (idx == 1) {
+                        //Intent scanqrIntent = new Intent(getApplication(), ShowQR.class);
+                        //startActivity(scanqrIntent);
+                   // }
+                    //else {
                         // nothing to do
-                    }
+                    //}
                 }
             });
 
             return alert.create();
         }
 
-        private void setMassage(String message) {
-            Pay pay = (Pay) getActivity();
-            pay.setTextView(message);
-        }
 
     }
 
