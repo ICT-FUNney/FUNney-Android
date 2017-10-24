b0f1214ebf7801d49b250f63e60378ae85313ae1
Keita <bonjovi1926@yahoo.co.jp>
2017-10-24 19:48:21 +0900

    add: Pay画面にNavigationDrawerを追加した

--- app/src/main/java/com/funney/funney_android/PayActivity.java
+++ app/src/main/java/com/funney/funney_android/PayActivity.java
@@ -5,13 +5,19 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class PayActivity extends AppCompatActivity {
+public class PayActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
 
     private DialogFragment dialogFragment;
@@ -22,8 +28,6 @@ public class PayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
 
-        assert getSupportActionBar() != null;
-        getSupportActionBar().setElevation(0);
 
         Button requestButton = (Button) findViewById(R.id.request);
         requestButton.setOnClickListener(new View.OnClickListener()
@@ -49,6 +53,19 @@ public class PayActivity extends AppCompatActivity {
                 dialogFragment.show(flagmentManager, "alert dialog");
             }
         });
+
+        // NavigationDrawer関連
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_pay);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_pay);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.pay_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
@@ -85,5 +102,31 @@ public class PayActivity extends AppCompatActivity {
 
             return alert.create();
         }
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_mining) {
+            // Handle the camera action
+        } else if (id == R.id.nav_edit_account) {
+            Intent intent = new Intent(getApplication(), EditAccountActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_agreement) {
+            Intent intent = new Intent(getApplication(), UserPolicy.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_question) {
+            Intent intent = new Intent(getApplication(), Question.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_logout) {
+            Intent intent = new Intent(getApplication(), Login.class);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_pay);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
