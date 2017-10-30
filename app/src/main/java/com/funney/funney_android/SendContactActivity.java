13d68d881404f7e6325e3b8bb1441bf43aecffe0
Keita <bonjovi1926@yahoo.co.jp>
2017-10-30 16:11:16 +0900

    fix: setTitle

--- app/src/main/java/com/funney/funney_android/SendContactActivity.java
+++ app/src/main/java/com/funney/funney_android/SendContactActivity.java
@@ -9,5 +9,9 @@ public class SendContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_contact);
+        android.support.v7.app.ActionBar actionbar = getSupportActionBar();
+        // 5.0以上でのToolbarの影消し
+        actionbar.setElevation(0f);
+        setTitle("Send Money");
     }
 }
